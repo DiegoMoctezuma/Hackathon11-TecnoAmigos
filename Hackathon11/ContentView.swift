a652889c45fad99b3128e419440eb369b58f0653
DiegoMoctezuma <moctezumarafael26@gmail.com>
2026-03-23 14:57:58 -0600

    Initial Commit

---
+++ Hackathon11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hackathon11
+//
+//  Created by Diego Moctezuma on 23/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hackathon11/Hackathon11App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon11App.swift
+//  Hackathon11
+//
+//  Created by Diego Moctezuma on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
